d485b5376206a029e8bac304a843b49b4abeb7e3
Emeyas Franco <emeyasdev@gmail.com>
2024-05-07 18:56:52 -0300

    atualização serviços

---
+++ script.js
@@ -0,0 +1,31 @@
+const fotos = document.querySelectorAll('.fotos div');
+const anterior = document.querySelector('.anterior');
+const proximo = document.querySelector('.proximo');
+
+let indiceAtual = 0;
+
+function mostrarFoto(indice) {
+    fotos.forEach(foto => foto.style.opacity = 0);
+    fotos[indice].style.opacity = 1;
+}
+
+mostrarFoto(indiceAtual);
+
+anterior.addEventListener('click', () => {
+    if (indiceAtual === 0) {
+        indiceAtual = fotos.length - 1;
+    } else {
+        indiceAtual--;
+    }
+    mostrarFoto(indiceAtual);
+});
+
+proximo.addEventListener('click', () => {
+    if (indiceAtual === fotos.length - 1) {
+        indiceAtual = 0;
+    } else {
+        indiceAtual++;
+    }
+    mostrarFoto(indiceAtual);
+});
+
